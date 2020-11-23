97e061c0adbd9cdecd6ad7f1dc67b2bf9169beee
Daniel Asante <baremetals16@gmail.com>
2020-11-23 11:23:50 +0000

    Redux Work

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import cartReducer from './reducer';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+var destination = document.querySelector("#container");
+var store = createStore(cartReducer);
+
+
+ReactDOM.render (
+    <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Provider>,
+    destination
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
